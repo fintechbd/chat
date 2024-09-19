2d5ed4cc6d2b1319898e23ec4f8b7d086b45fc56
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-19 20:17:29 +0000

    Fix styling

--- src/Chat.php
+++ src/Chat.php
@@ -12,24 +12,24 @@ class Chat
      * @return ChatGroupService
      */
     public function chatGroup($filters = null)
-{
-	return \singleton(ChatGroupService::class, $filters);
+    {
+        return \singleton(ChatGroupService::class, $filters);
     }
 
     /**
      * @return ChatParticipantService
      */
     public function chatParticipant($filters = null)
-{
-	return \singleton(ChatParticipantService::class, $filters);
+    {
+        return \singleton(ChatParticipantService::class, $filters);
     }
 
     /**
      * @return ChatMessageService
      */
     public function chatMessage($filters = null)
-{
-	return \singleton(ChatMessageService::class, $filters);
+    {
+        return \singleton(ChatMessageService::class, $filters);
     }
 
     //** Crud Service Method Point Do not Remove **//
