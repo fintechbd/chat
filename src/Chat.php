f67a6a7f1cfea1da2faaaeae31709c89e6c9d78b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-20 02:37:57 +0600

    unused php doc block removed

--- src/Chat.php
+++ src/Chat.php
@@ -8,25 +8,16 @@ use Fintech\Chat\Services\ChatParticipantService;
 
 class Chat
 {
-    /**
-     * @return ChatGroupService
-     */
     public function chatGroup($filters = null)
     {
         return \singleton(ChatGroupService::class, $filters);
     }
 
-    /**
-     * @return ChatParticipantService
-     */
     public function chatParticipant($filters = null)
     {
         return \singleton(ChatParticipantService::class, $filters);
     }
 
-    /**
-     * @return ChatMessageService
-     */
     public function chatMessage($filters = null)
     {
         return \singleton(ChatMessageService::class, $filters);
